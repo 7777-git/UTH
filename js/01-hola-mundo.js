0d10203dbea3c0c2b581b76cc886ff81db4caf87
7777-git <anton77maan@outlook.es>
2024-11-13 17:42:37 +0100

    Add files via upload

---
+++ js/01-hola-mundo.js
@@ -0,0 +1,10 @@
+/*  alert("Hola mundo con JS");
+alert("Bienvenido con JS");
+alert("Peasssso con JS");
+
+document.write("Que pasa peñita");
+
+console.log("Muestra esto en la consola");  */
+
+
+document.write("Que pasa peñitaaaa");
